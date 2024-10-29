c96759ea1141e67de29104865b23ff9076fc7c81
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-29 16:27:12 +0100

    added template header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,17 @@
 <footer>
 
-    <div>Footer goes here</div>
+    <nav class="nav-footer">
+        <div class="logo">supercar</div>
+        <div class="footer-links">
+            <ul>
+                <li><a class="nav-item" href="#">about us</a></li>
+                <li><a class="nav-item" href="#">conttact us</a></li>
+                <li><a class="nav-item" href="#">ours socials</a></li>
+                <li><a class="nav-item" href="#">privacy policy</a></li>
+                <li><a class="nav-item" href="#">terms & conditions</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- here the scroll back btn -->
+    <a href="#" class="btn"></a>
 </footer>
