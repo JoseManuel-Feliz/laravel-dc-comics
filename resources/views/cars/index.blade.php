82edf61f153087e2b48c71c387652cbb9ec607c1
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-29 23:52:11 +0100

    fix: added cards and remove list items

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -7,33 +7,33 @@
 <div>here goes the main content</div>
 
 
+<div class="container">
+    <div class="row">
 
-<!-- "name" => "Lamborghini Sian FKP 37"
-"brand" => "Lamborghini"
-"engine" => "6.5L V12 Hybrid"
-"top_speed" => 350
-        "power" => 819
-        "country" => "Italy"
-        "image_url" => "https://cdn.motor1.com/images/mgl/MZp7Y/s1/lamborghini-sian-fkp-37.jpg"
-        "weight" => "1620.00"
-        "description" => "La prima supercar ibrida di Lamborghini, dotata di tecnologie innovative per prestazioni incredibili." -->
-@forelse($cars as $car)
-<ul>
-    <li>{{$car->name}}</li>
-    <li>{{$car->brand}}</li>
-    <li>{{$car->engine}}</li>
-    <li>{{$car->top_speed.' km/h'}}</li>
-    <li>{{$car->power.' HP'}}</li>
-    <li>{{$car->country}}</li>
-    <li><img src="{{$car->image_url}}" alt="{{$car->name.' photo'}}"></li>
-    <li>{{$car->weight.' kg'}}</li>
-    <li>{{$car->description}}</li>
+        @forelse($cars as $car)
+        <div class="col">
 
-</ul>
-@empty
-<ul>
-    <li>no cars available</li>
-</ul>
-@endforelse
+            <a class="card" href="#">
+                <div class="img-wrapper">
+                    <img src="{{$car->image_url}}" alt="{{$car->name.' photo'}}">
+                </div>
+                <h5 class="brand-model">{{$car->name}}</h5>
+                <p>{{$car->brand}}</p>
+                <p>{{$car->engine}}</p>
+                <p>{{$car->top_speed.' km/h'}}</p>
+                <p>{{$car->power.' HP'}}</p>
+                <p>{{$car->country}}</p>
+                <p>{{$car->weight.' kg'}}</p>
+                <p>{{Str::words($car->description,5,' ...')}}</p>
+            </a>
 
+        </div>
+        @empty
+        <div class="col">
+            <p>no cars available</p>
+        </div>
+        @endforelse
+
+    </div>
+</div>
 @endsection
