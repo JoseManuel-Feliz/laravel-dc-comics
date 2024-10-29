b087cc373ee65d88cf4f6c2eb570bdbc7e346a57
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-29 17:19:16 +0100

    added cars model and migration

---
+++ database/migrations/2024_10_29_155429_create_cars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50)->nullable(false);
+            $table->string('brand', 40)->nullable(false);
+            $table->string('engine', 50)->nullable(true);
+            $table->tinyInteger('top_speed')->unsigned(true)->nullable(true);
+            $table->tinyInteger('power')->unsigned(true)->nullable(true);
+            $table->string('country', 20)->nullable(false);
+            $table->text('image_url', 20)->nullable(true);
+            $table->float('weight', 5, 2)->nullable(true);
+            $table->text('description', 20)->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
