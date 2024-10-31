8f2b6fcf8a94105270a52e403661d3b46752232f
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-31 14:36:48 +0100

    added default values to cars table columns

---
+++ database/migrations/2024_10_30_233754_update_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->string('engine_fuel_type', 50)->nullable(true)->change();
+            $table->float('engine_hp')->nullable(true)->default(0)->change();
+            $table->float('engine_cylinders')->nullable(true)->default(0)->change();
+            $table->tinyInteger('highway_mpg')->unsigned()->nullable(true)->default(0)->change();
+            $table->tinyInteger('city_mpg')->unsigned()->nullable(true)->default(0)->change();
+            $table->integer('popularity')->unsigned()->nullable(true)->default(0)->change();
+            $table->integer('msrp')->unsigned()->nullable(true)->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            //
+        });
+    }
+};
