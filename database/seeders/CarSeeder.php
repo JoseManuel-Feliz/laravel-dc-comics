7f1df6fe8d8224067b672f215debf84ca38adf98
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-31 15:50:50 +0100

    fix: import all cars data from car_data.csv in Car seeder

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -23,15 +23,44 @@ class CarSeeder extends Seeder
         while (($row = fgetcsv($file)) !== false) {
 
             $carData = [
+
+                /* | FUNCTIONS TO SEARCH THE ARRAY FOR VALUES IN EACH COLUMN */
+
+                /* |  */
                 'make' => $row[array_search('Make', $tableColumns)],
                 'model' => $row[array_search('Model', $tableColumns)],
                 'year' => $row[array_search('Year', $tableColumns)],
+
+                /* | */
+                'engine_fuel_type' => $row[array_search('Engine Fuel Type', $tableColumns)],
+                'engine_hp' => $row[array_search('Engine HP', $tableColumns)],
+                'engine_cylinders' => $row[array_search('Engine Cylinders', $tableColumns)],
+
+                /* | */
+                'transmission_type' => $row[array_search('Transmission Type', $tableColumns)],
+                'driven_wheels' => $row[array_search('Driven_Wheels', $tableColumns)],
+                'number_of_doors' => $row[array_search('Number of Doors', $tableColumns)],
+
+                /* | */
+                'market_category' => $row[array_search('Market Category', $tableColumns)],
+                'vehicle_size' => $row[array_search('Vehicle Size', $tableColumns)],
+                'vehicle_style' => $row[array_search('Vehicle Style', $tableColumns)],
+
+                /* | */
+                'highway_mpg' => $row[array_search('highway MPG', $tableColumns)],
+                'city_mpg' => $row[array_search('city mpg', $tableColumns)],
+                'popularity' => $row[array_search('Popularity', $tableColumns)],
+
+                /* | */
+                'msrp' => $row[array_search('MSRP', $tableColumns)],
+                'description' => $row[array_search('description', $tableColumns)],
+                'image_url' => $row[array_search('image_url', $tableColumns)],
 
             ];
 
             $carData = Car::create($carData);
 
-            var_dump($carData);
+            /* dd($carData); */
         }
 
 
